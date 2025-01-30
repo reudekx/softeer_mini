b2da4a1346878db0a3d50b058f8241ae4d1dd05a
reudekx <reudekx@gmail.com>
2025-01-30 20:33:19 +0900

    feat: add codes to extract emile data

--- app/main.py
+++ app/main.py
@@ -36,9 +36,28 @@ def test_kim_min_jae():
 
     print(fbref_data)
 
+def test_emile_smith_rowe():
+    site = extractor.Site.FOTMOB
+    url = "https://www.fotmob.com/ko/players/889534/emile-smith-rowe"
+    name = "emile-smith-rowe"
+
+    fotmob_data = extractor.extract(site, url, name)
+
+    site = extractor.Site.FBREF
+    url = "https://fbref.com/en/players/17695062/Emile-Smith-Rowe"
+    name = "emile-smith-rowe"
+
+    fbref_data = extractor.extract(site, url, name)
+
+    print(fotmob_data)
+
+    print(fbref_data)
+
 
 if __name__ == "__main__":
 
-    test_kim_min_jae()
+    # test_kim_min_jae()
 
     # test_son_heung_min()
+
+    test_emile_smith_rowe()
